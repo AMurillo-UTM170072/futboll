3ea0fb1b747e68ffb15d055312287e4aa2e44bd1
AMurillo-UTM170072 <aureliomurillo12@gmail.com>
2022-11-05 15:56:47 -0600

    refactor

--- vistas/models.py
+++ vistas/models.py
@@ -6,3 +6,18 @@ class Tabla_test(models.Model):
     columna_dos = models.CharField(max_length=200)
     columna_tres = models.CharField(max_length=200)
     columna_cuatro = models.CharField(max_length=200)
+
+class Rol(models.Model):
+    rol_id = models.AutoField(primary_key=True)
+    Rol = models.CharField(max_length=200)
+    Descripcion = models.CharField(max_length=200)
+
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    Nombre = models.CharField(max_length=200)
+    ApellidoPaterno = models.CharField(max_length=200)
+    ApellidoPaterno = models.CharField(max_length=200)
+    Curp = models.CharField(max_length=200)
+    Brithday = models.DateTimeField()
+   
+    
