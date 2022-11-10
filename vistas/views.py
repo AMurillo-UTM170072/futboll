2ac6f76e69be5dbc8ed5ef932f5efe869b5bd96d
AMurillo-UTM170072 <aureliomurillo12@gmail.com>
2022-11-10 06:19:51 -0600

    refactor: many changes

--- vistas/views.py
+++ vistas/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+from django.template import Template,Context
+import os 
+# Create your views here.import os
 def inicio(resquest):
     return HttpResponse("<h1>Liga de futboll</h1>")
 
 def index (request):
     context = {'foo': 'bar'}
     return render(None, 'base.html', context)
+def admin(request):
+    cardItem = [
+        {'Nombre':'Alta de jugador'},
+        {'Nombre':'Equipos'},
+        {'Nombre':'Torneo'},
+        {'Nombre':'Arbitros'}
+    ]
+    return render(request,'pages/adminHome/homeAdmin.html',{'dict':cardItem})
+
 def jugadores (request):
     return render (request,'pages/jugadores/jugadores.html')
 def infoJugadores (request):
