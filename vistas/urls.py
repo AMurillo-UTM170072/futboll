d6594fc159ff68a8af31e1a5b9a9baaefca2e47a
AMurillo-UTM170072 <aureliomurillo12@gmail.com>
2023-03-04 10:41:48 -0600

    many changes

--- vistas/urls.py
+++ vistas/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path ('jugadores',views.jugadores ,name='jugadores'),
+    path ('equipo',views.infoEquipo ,name='equipoAlta'),
     path ('directivo',views.adminJournament  ,name='adminHome'),
     path ('directivo/Torneos',views.getJournament ,name='directivoTorneo'),
     path ('jugadores/infoJugador',views.infoJugadores ,name='infoJugadores'),
+    path ('directivo/equipoAlta',views.altaEquipo ,name='altaEquipo'),
     path ('directivo/torneoAlta',views.altaTorneo ,name='altaTorneo'),
-    path ('directivo/torneoAlta',views.altaTorneo,name="altaTorneo")
+    path ('jugadores/jugadorAlta',views.altaJugador,name="altaJugador")
 ]
--- vistas/views.py
+++ vistas/views.py
@@ -4,6 +4,12 @@ from django.template import Template,Context
 from .forms import Liguilla
 from .models import Torneo
 import os 
+cardItem = [
+    {'Nombre':'Alta de jugador',"url":"https://www.playerone.vg/wp-content/uploads/2021/05/ReZero-kara-Hajimeru-Isekai-Seikatsu-Hyouketsu-no-Kizuna-finaliza-e1622492984100.jpg","path":"infoJugadores"},
+    {'Nombre':'Equipos',"url":"https://www.playerone.vg/wp-content/uploads/2021/05/ReZero-kara-Hajimeru-Isekai-Seikatsu-Hyouketsu-no-Kizuna-finaliza-e1622492984100.jpg","path":"equipoAlta"},
+    {'Nombre':'Torneo',"url":"https://www.playerone.vg/wp-content/uploads/2021/05/ReZero-kara-Hajimeru-Isekai-Seikatsu-Hyouketsu-no-Kizuna-finaliza-e1622492984100.jpg","path":"directivoTorneo"},
+    {'Nombre':'Arbitros',"url":"https://www.playerone.vg/wp-content/uploads/2021/05/ReZero-kara-Hajimeru-Isekai-Seikatsu-Hyouketsu-no-Kizuna-finaliza-e1622492984100.jpg","path":"infoJugadores"}
+]
 # Create your views here.import os
 def inicio(resquest):
     return HttpResponse("<h1>Liga de futboll</h1>")
@@ -12,16 +18,9 @@ def index (request):
     context = {'foo': 'bar'}
     return render(None, 'base.html', context)
 def adminJournament(request):
-    cardItem = [
-        {'Nombre':'Alta de jugador',"url":"https://www.playerone.vg/wp-content/uploads/2021/05/ReZero-kara-Hajimeru-Isekai-Seikatsu-Hyouketsu-no-Kizuna-finaliza-e1622492984100.jpg","path":"infoJugadores"},
-        {'Nombre':'Equipos',"url":"https://www.playerone.vg/wp-content/uploads/2021/05/ReZero-kara-Hajimeru-Isekai-Seikatsu-Hyouketsu-no-Kizuna-finaliza-e1622492984100.jpg","path":"infoJugadores"},
-        {'Nombre':'Torneo',"url":"https://www.playerone.vg/wp-content/uploads/2021/05/ReZero-kara-Hajimeru-Isekai-Seikatsu-Hyouketsu-no-Kizuna-finaliza-e1622492984100.jpg","path":"directivoTorneo"},
-        {'Nombre':'Arbitros',"url":"https://www.playerone.vg/wp-content/uploads/2021/05/ReZero-kara-Hajimeru-Isekai-Seikatsu-Hyouketsu-no-Kizuna-finaliza-e1622492984100.jpg","path":"infoJugadores"}
-    ]
     return render(request,'pages/adminHome/homeAdmin.html',{'dict':cardItem})
 def getJournament (request):
     tournament = Torneo.objects.all()
-    print('inicio de torneo',tournament)
     return render(request,'pages/adminHome/adminTableTorneos.html',{'tournament':tournament} )
 def navegarOtro(request):
     return render('pages/adminHome/homeAdmin.html',{'dict':cardItem})
@@ -29,6 +28,10 @@ def jugadores (request):
     return render (request,'pages/jugadores/jugadores.html')
 def infoJugadores (request):
     return render (request,'pages/jugadores/tableInfoJugadores.html')
+def infoEquipo (request):
+    return render (request,'pages/Equipos/tableInfoEquipos.html')
+def altaEquipo (request):
+    return render (request,'pages/Equipos/tableInfoEquipos.html')
 def alta (request):
     return render (request,'pages/jugadores/tableInfoJugadores.html')
 def altaTorneo(request):
@@ -36,3 +39,5 @@ def altaTorneo(request):
     if formulario.is_valid():
         formulario.save()
     return render(request,"pages/torneoForm/formTemplate.html",{"formulario":formulario})
+def altaJugador(request):
+    return render(request,"pages/jugadores/jugadoresAlta.html")
